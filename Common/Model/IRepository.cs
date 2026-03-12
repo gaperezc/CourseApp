145660f379a31b6d3fb45818f8f5705d2fd086a7
Gelacio Perez <gelacio_perez@homail.com>
2026-03-12 17:22:07 -0600

    feat: add IRepository interface for Aggregate Root

---
+++ Common/Model/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq.Expressions;
+
+namespace Common.Model
+{
+    public interface IRepository<T> where T : AggregateRoot
+    {
+        T GetById(Guid id);
+
+        T Find(Expression<Func<T, bool>> match);
+        IEnumerable<T> GetAll(Expression<Func<T, bool>> match);
+
+        void Add(T entity);
+    
+        void Update(T entity);
+    
+        void Delete(Guid id);
+    }
+}
---
+++ CoursesApp.Domain/Security/RoleAggregate/IRoleRepository.cs
@@ -0,0 +1,15 @@
+﻿using Common.Model;
+
+namespace CoursesApp.Domain.Security.RoleAggregate
+{
+    internal interface IRoleRepository: IRepository<Role>
+    {
+            List<Role> GetByStatus(RoleStatus status);
+
+            void addUser(Role user);
+
+           Role GetUserById(Guid id);
+
+           Role GetUserByCode(string code);
+    }
+}
