7fece1e59543a4bf38d9391ad1c5c98f546dcb2f
Gelacio Perez <gelacio_perez@homail.com>
2026-03-12 18:02:42 -0600

    feat: add basic domain validation rules for Role entity using FluentValidation

---
+++ CoursesApp.Domain/Security/RoleAggregate/RoleValidation.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+
+namespace CoursesApp.Domain.Security.RoleAggregate
+{
+    public class RoleValidation : AbstractValidator<Role>
+    {
+        public RoleValidation()
+        {
+            RuleFor(x => x.Id)
+                .NotNull().WithMessage("Id cannot be null");
+
+            RuleFor(x => x.Name)
+                .NotNull().WithMessage("Code cannot be null")
+                .NotEmpty().WithMessage("Code cannot be empty")
+                .MaximumLength(20).WithMessage("Code cannot exceed 20 characters");
+
+            RuleFor(x => x.Name)
+                .NotNull().WithMessage("Name cannot be null")
+                .NotEmpty().WithMessage("Name cannot be empty")
+                .MaximumLength(50).WithMessage("Name cannot exceed 50 characters");
+
+            RuleFor(x => x.status)
+                .NotNull().WithMessage("Status cannot be null");
+
+            RuleFor(x => x.Description)
+                .NotNull().WithMessage("Description cannot be null")
+                .MaximumLength(500).WithMessage("Description cannot exceed 500 characters");
+
+
+
+        }
+    }
+}
